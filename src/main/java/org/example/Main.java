0535b4e191a7a7acbb06d0b25d1b281e02ed6bc6
Usman <usman@Usmans-MacBook-Air.local>
2023-03-25 14:03:47 -0400

    first commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,88 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        // Whenever we have a variable, we need to start with datatype, then variable name then assign value to it
+        String name = "Usman";
+        String address = "My name is Usman, and i Live at #$#$ 99 hollybrooke";
+        System.out.println(name);
+
+        /* We have two different types of data type ( Primitive, Non-Primitive)
+        // Primitive data types - includes byte , short , int , long , float , double , boolean and char.
+        Non-primitive data types - String
+        String name = "Usman"
+        System.out.println("I love coding");
+        */
+
+        /* asdflaksdfj */
+        // lkajsdlfjasfj
+        // alksdjflasdjfkl
+
+        int num = 234;
+        System.out.println(num);
+        System.out.println(num);
+
+        char ch = '1';
+        char ab = 'a';
+        char ba = 'y';
+
+        System.out.println(ch);
+        System.out.println(ba);
+        System.out.println(ab);
+
+        // a float value can hold only a maximum of 7 digits after the decimal
+        float tr = 2.434f;
+        System.out.println(tr);
+
+        // We should use int datatype for Any integer value which is less than 8 digits
+        int bigVal = 12345;
+        System.out.println(bigVal);
+        // Integer between 8 decimals to 15 decimals. You should go for double data type
+        double bigValue = 1234578;
+        System.out.println(bigValue);
+
+        //boolean
+        boolean isTrue = true;
+        boolean abcdd = true;
+        boolean isFalse = false;
+        boolean fdsf = false;
+
+        System.out.println(isTrue);
+        System.out.println(isFalse);
+
+        System.out.println("Hello world!");
+        System.out.println("Hello Universe");
+        System.out.println(45);
+
+        //Concatenation of two strings
+        String n0 = "Name: ";
+        String n1 = "My name";
+        String n2 = " is Muhammad Usman";
+        System.out.println(n0 + n1 + n2);
+        System.out.println(n1 + " is Muhammad Usman");
+
+        //Concatenation of int and string
+        String abn = "My number is ";
+        int n34 = 45;
+        System.out.println(abn + n34);
+
+
+        String av = "45";
+        int va = 45;
+        int baa = 5;
+        System.out.println(av + va + baa);
+
+        System.out.println(va + baa);
+
+        int hj = 4 + 10;
+        System.out.println(hj / 7);
+        System.out.println(5 * 3);
+
+        int kl = 34 - hj;
+        System.out.println(kl);
+
+
+    }
+}
